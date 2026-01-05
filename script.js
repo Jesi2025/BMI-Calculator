f24e78cda71593816d27920af5ee27db88bf9c4c
Jesinthasam <jesintha@wisright.com>
2026-01-05 15:40:00 +0530

    Bmi calculator

---
+++ script.js
@@ -0,0 +1,45 @@
+function calculateBMI(){
+    let weightInput = prompt("Enter your weight in kg:");
+    let heightInput = prompt("Enter your height in cm:");
+
+    let weight = parseFloat(weightInput);
+    let heightCm = parseFloat(heightInput);
+
+    
+    let height = heightCm / 100;
+
+    let bmi = weight / (height * height);
+
+
+    let resultMessage = `Your BMI is: ${bmi.toFixed(2)}<br>`;
+
+
+if(bmi < 18.5){
+    resultMessage += "You are underweight";
+
+}
+else if(bmi >=18.5 && bmi <=24.9){
+    resultMessage += "You are Healthy";
+
+}
+else if(bmi >=25 && bmi <=29.9){
+    resultMessage += "Your are  over weight";
+    
+}
+else{
+    resultMessage += "You are obese";
+}
+
+
+document.getElementById("result").innerHTML = resultMessage;
+}
+
+
+let number = 5;
+let number1 = 3;
+
+console.log(number + number1);
+
+let fruits = ["apple","banana","orange"];
+
+fruits.push("banana");
